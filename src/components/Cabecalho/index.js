05b5c1762b769d36693bbf5717f8f34a6c577930
braiancalpar <braian.santos@calpar.com.br>
2026-06-25 13:15:40 -0300

    first commit

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,20 @@
+import styles from "./Cabecalho.module.css";
+import { Link } from "react-router-dom";
+import logo from "./logo.png";
+import CabecalhoLink from "../CabecalhoLink";
+
+function Cabecalho() {
+  return (
+    <header className={styles.cabecalho}>
+      <Link to="./">
+        <img src={logo} alt="Logo do cinetag" />
+      </Link>
+      <nav>
+        <CabecalhoLink url="./">Home</CabecalhoLink>
+        <CabecalhoLink url="./favoritos">Favoritos</CabecalhoLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Cabecalho;
---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,13 @@
+import Rodape from "components/Rodape";
+import Cabecalho from "../../components/Cabecalho";
+
+function Inicio() {
+  return (
+    <>
+      <Cabecalho />
+      <Rodape />
+    </>
+  );
+}
+
+export default Inicio;
